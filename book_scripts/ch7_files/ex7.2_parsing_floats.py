d9ac3dda39f99a22e44bbaf8e17ef3de6545b912
Ivan Konovalenko <konovalenko11@gmail.com>
2018-11-18 21:37:37 -0800

    7 chapter fix

--- book_scripts/ch7_files/ex7.2_parsing_floats.py
+++ book_scripts/ch7_files/ex7.2_parsing_floats.py
@@ -1,12 +1,18 @@
 """
-    Exercise 7.1
+    Exercise 7.2
 
-        Write a program to read through a file and print the contents of the
-        file (line by line) all in upper case. Executing the program will look as follows:
-        python shout.py
-        Enter a file name: mbox-short.txt
-        FROM STEPHEN.MARQUARD@UCT.AC.ZA SAT JAN 5 09:14:16 2008
-        RETURN-PATH: <POSTMASTER@COLLAB.SAKAIPROJECT.ORG>
+        Write a program to prompt for a file name, and then read through the file and look for lines of the form:
+            X-DSPAM-Confidence: 0.8475
+        When you encounter a line that starts with “X-DSPAM-Confidence:”
+        pull apart the line to extract the floating-point number on the line.
+        Count these lines and then compute the total of the spam confidence values from these lines.
+        When you reach the end of the file, print out the average spam confidence.
+
+        Enter the file name: mbox.txt
+        Average spam confidence: 0.894128046745
+        
+        Enter the file name: mbox-short.txt
+        Average spam confidence: 0.750718518519
 
 """
 
